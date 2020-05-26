7ed808b1bee1d380cda4664f84ece3a0ae168e1d
范滇东 <1525876733@qq.com>
2020-05-26 21:35:35 +0800

    Change to POST

--- manim/api.php
+++ manim/api.php
@@ -1,3 +1,15 @@
 <?php
-echo file_get_contents("http://127.0.0.1:2333/?".$_SERVER['QUERY_STRING']);
+//echo file_get_contents("http://127.0.0.1:2333/?".$_SERVER['QUERY_STRING']);
+$s=http_build_query($_POST);
+$options = array(
+	'http' => array(
+	  'method' => 'POST',
+	  'header' => 'Content-type:application/x-www-form-urlencoded',
+	  'content' => $s,
+	  'timeout' => 15 * 60
+)
+);
+$context = stream_context_create($options);
+$result = file_get_contents("http://127.0.0.1:2333", false, $context);
+echo $result;
 ?>
